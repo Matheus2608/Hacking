4bacd0cb6cfa3e7cf0ac8cf35ff60369436a3c47
Matheus2608 <matheuslacerdadasilveira@gmail.com>
2024-08-22 13:04:25 -0300

    parse body req and make http req using local proxy

--- intruder/api/handlers/handlers.go
+++ intruder/api/handlers/handlers.go
@@ -1,20 +1,58 @@
 package handlers
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
-// type Response struct {
-// 	requestId        int
-// 	payload          string
-// 	statusCode       int
-// 	responseReceived int
-// 	err              string
-// 	timeout          bool
-// 	length           int
-// 	comment          string
-// }
+func makeRequest(req string) (*http.Request, error) {
+	path, requestMap := parseRequest(req)
+
+	host, hostExists := requestMap["Host"]
+	if !hostExists {
+		fmt.Println("Host key does not exist in requestMap")
+		return nil, fmt.Errorf("host key does not exist in requestMap")
+	}
+
+	// Ensure the URL is properly formatted
+	url := "https://" + host + path
+
+	httpReq, err := http.NewRequest(http.MethodGet, url, nil)
+	if err == nil {
+		for headerType, value := range requestMap {
+			httpReq.Header.Add(headerType, value)
+		}
+	}
+
+	return httpReq, err
+}
+
+func parseRequest(req string) (string, map[string]string) {
+	requestMap := make(map[string]string)
+
+	lines := strings.Split(req, "\n")
+	path := strings.SplitN(lines[0], " ", 3)[1]
+
+	for _, header := range lines[1:] {
+		if len(header) < 3 {
+			break
+		}
+
+		headerList := strings.SplitN(header, ": ", 2)
+		if len(headerList) == 2 {
+			typeHeader, value := headerList[0], headerList[1]
+			requestMap[typeHeader] = strings.TrimSpace(value)
+		} else {
+			fmt.Println("Invalid header format:", header)
+		}
+	}
+
+	return path, requestMap
+}
 
 func PostHandler(res http.ResponseWriter, req *http.Request) {
 	// Parse the form data
@@ -26,7 +64,37 @@ func PostHandler(res http.ResponseWriter, req *http.Request) {
 	// Access the form values
 	params := req.Form
 
-	for key, value := range params {
-		fmt.Println(key, value)
+	httpReq, err := makeRequest(params["requestData"][0])
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
 	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(&url.URL{
+				Scheme: "http",
+				Host:   "127.0.0.1:8080",
+			}),
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+
+	// Send the request
+	httpRes, err := client.Do(httpReq)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+
+	// Read the response body
+	body, err := io.ReadAll(httpRes.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+	}
+
+	bodyString := string(body)
+	fmt.Println(bodyString)
+
+	httpRes.Body.Close()
 }
